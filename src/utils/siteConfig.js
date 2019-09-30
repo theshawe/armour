482478b47614e65013093bfa5557158d88a7b188
theshawe <david@invisiblebuilding.com>
2019-09-30 13:05:10 +0100

    edited site config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Armour | Legislation made effortless`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Identifies legislation applicable to your company in minutes. Contains no legal jargon, simple key fact statements, no fuss, effortless.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Armour`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
